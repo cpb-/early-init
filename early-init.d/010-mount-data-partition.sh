64b774903f8a852c460a2532c17b79a6b0bbc08e
Christophe Blaess <christophe.blaess@logilin.fr>
2024-02-17 11:21:40 +0100

    [DEV] Improve 010 example script.

--- early-init.d/010-mount-data-partition.sh
+++ early-init.d/010-mount-data-partition.sh
@@ -3,31 +3,41 @@
 PARTITION=/dev/mmcblk0p3
 MOUNTPOINT=/data
 FILESYSTEM=ext4
+NAME=data
 
-if mount ${PARTITION} ${MOUNTPOINT} -t ${FILESYSTEM}
+printf "Mounting ${NAME} partition..." >&2
+if mount ${PARTITION} ${MOUNTPOINT} -t ${FILESYSTEM} >/dev/null 2>&1
 then
+	printf "Ok\n" >&2
 	exit 0
 fi
+printf "Failed\n" >&2
 
-if fsck.${FILESYSTEM}  -p  ${PARTITION}
+printf "Checking ${NAME} partition..." >&2
+if fsck.${FILESYSTEM}  -p  ${PARTITION} >/dev/null 2>&1
 then
-	if mount ${PARTITION} ${MOUNTPOINT} -t ${FILESYSTEM}
+	if mount ${PARTITION} ${MOUNTPOINT} -t ${FILESYSTEM} >/dev/null 2>&1
 	then
+		printf "Ok\n" >&2
 		exit 0
 	fi
 fi
+printf "Failed\n" >&2
 
+printf "Formating ${NAME} partition..." >&2
 if [ "${FILESYSTEM}" = "vfat" ]
 then
-	mkfs.vfat -c -n DATA ${PARTITION} 2>/dev/null
+	mkfs.vfat -c -n DATA ${PARTITION} >/dev/null 2>&1
 else
-	mkfs.${FILESYSTEM} -c -L DATA -q ${PARTITION} 2>/dev/null
+	mkfs.${FILESYSTEM} -c -L DATA -q ${PARTITION} >/dev/null 2>&1
 fi
 
 
-if mount ${PARTITION} ${MOUNTPOINT} -t ${FILESYSTEM}
+if mount ${PARTITION} ${MOUNTPOINT} -t ${FILESYSTEM} >/dev/null 2>&1
 then
+	printf "Ok\n" >&2
 	exit 0
 fi
 
+printf "Failed\n" >&2
 exit 1
