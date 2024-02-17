a6de45f6885b27c0d15e83e1ca5db665ceae92a9
Christophe Blaess <christophe.blaess@logilin.fr>
2024-02-17 11:28:16 +0100

    [DEV] Imprive 020 example script.

--- early-init.d/020-mount-overlayfs-on-etc.sh
+++ early-init.d/020-mount-overlayfs-on-etc.sh
@@ -2,6 +2,7 @@
 
 WORKDIR=/data/overlayfs/etc/workdir
 UPPERDIR=/data/overlayfs/etc/upperdir
+
 mkdir -p "${WORKDIR}"  "${UPPERDIR}"
 
 mount none /etc -t overlay -o lowerdir=/etc,workdir="${WORKDIR}",upperdir="${UPPERDIR}"
